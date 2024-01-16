36db50f9be21e01b5db924e80d7fecf1cd8e31ec
Eslam Elsharkawy <abstract@pop-os.localdomain>
2024-01-16 20:44:19 +0200

    added convs_list

--- install.bash
+++ install.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp ./pythonrc.py $HOME/.pythonrc.py
+cp ./pythonrc.py $HOME/.pythonrc.py && cp -r ./pythonrc_data $HOME
 echo 'export PYTHONSTARTUP=$HOME/.pythonrc.py' >> $HOME/.bashrc
--- pythonrc.py
+++ pythonrc.py
@@ -4,6 +4,9 @@ import json
 import os
 
 
+conv_json_obj: dict = {}
+
+
 def print_members(obj):
     members = dir(obj)
     json_str = json.dumps(members, indent=2)
@@ -15,6 +18,47 @@ def rand_str(str_len: int) -> str:
     return ''.join([random.choice(str_source) for _ in range(str_len)])
 
 
+def convs_list(key: str = None, rand: bool = False) -> list[str]:
+    if key == None:
+        print(list(conv_json_obj.keys()))
+        return
+
+    key_parts = key.split('.')
+    if len(key_parts) == 1:
+        part_1 = key_parts[0]
+        part_2_dict: dict = conv_json_obj[part_1]
+        part_2_keys = list(part_2_dict.keys())
+        print(part_2_keys)
+
+        if rand:
+            print('=' * 100)
+            print(f"CHOICE: {random.choice(part_2_keys)}")
+            print('=' * 100)
+        return
+
+    if len(key_parts) == 2:
+        part_1, part_2 = key_parts
+        vals_list = conv_json_obj[part_1][part_2]
+        for q in vals_list:
+            print(q)
+
+        if rand:
+            print('=' * 100)
+            print(f"CHOICE: {random.choice(vals_list)}")
+            print('=' * 100)
+        return
+
+
+def gen_convs_graph(top_key: str):
+    # TODO: using AI generate knowledge graph from convs.json file
+    pass
+
+
 print(f"Loaded: {os.environ['PYTHONSTARTUP']}")
+
+with open('pythonrc_data/convs.json', 'r') as f:
+    conv_json_obj = json.loads(f.read())
+    print("Loaded conv.json file")
+
 # print runtime symbols
 print(json.dumps([func for func in dir() if callable(globals()[func]) and not func.startswith("__")], indent=2))
